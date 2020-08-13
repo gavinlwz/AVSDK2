d56665e5a678b0034d5d789898e1e22a4e165421
shuhuan <shuhuan_bruce@icloud.com>
2020-08-13 18:10:57 +0800

    更新gradle版本

--- youme_voice_engine/platforms/Android/java/build/intermediates/java_res/debug/out/com/youme/engine/buildh.sh
+++
@@ -1,5 +0,0 @@
-javah -classpath ../../../../bin/classes -jni engineveEngine
-cp ./com_youme_voiceengine_NativeEngine.h ../../../../jni/com_youme_voiceengine_NativeEngine.h
-
-javah -classpath ../../../../bin/classes -jni enginecp ./com_youme_voiceengine_api.h ../../../../jni/com_youme_voiceengine_api.h
-
